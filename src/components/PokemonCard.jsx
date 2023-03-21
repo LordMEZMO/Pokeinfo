0b1791295f41757927642b7c0947a7a4b49d26b0
Michal <michalgra25@gmail.com>
2023-03-21 09:21:53 +0100

    get pokemon sprites

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -7,13 +7,15 @@ function PokemonCard({name, link}) {
 	const capitalize = (text) => text.at(0).toUpperCase() + text.slice(1)
 
 	let [types, setTypes] = useState([]);
+	let [sprite, setSprite] = useState("")
 	
 	useEffect(() => {
 		const pokedex = new Pokedex();
+
 		let getPokemonData = async () => {
             const data = await pokedex.getPokemonByName(name)
 			setTypes(data.types)
-			console.log(data);
+			setSprite(data.sprites.front_default)
         }
 
 		getPokemonData()
@@ -21,17 +23,21 @@ function PokemonCard({name, link}) {
 	
 
   return (
-	<div className='card'>
+	<div className='card is-flex is-flex-direction-column is-justify-content-space-between'>
+		<div className="card-image">
+		<figure className="image">
+      		<img src={sprite} alt=""/>
+    	</figure>
+		</div>
 		<div className="card-header">
 			<h5 className="card-header-title">
 				<a href={link}>{capitalize(name)}</a>
 			</h5>
 		</div>
-		<div className='card-footer tags are-small'>
+		<div className='card-footer are-small'>
 			{
-				types.map((n)=> {
-					console.log(n);
-					return <PokemonType type={capitalize(n.type.name)}/>
+				types.map((n, key)=> {
+					return <PokemonType type={capitalize(n.type.name)} key={key}/>
 				})
 			}
 		</div>
