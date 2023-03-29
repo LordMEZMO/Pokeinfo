724445586cab714a5c0f90a410dba17b7164a996
Mabrams40 <maciej.abrams@gmail.com>
2023-03-29 11:32:55 +0200

    Minor fixes

--- src/components/SearchOptions.jsx
+++ src/components/SearchOptions.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SearchOptions({ handleSearchByName }) {
     return (
         <div className="search control" style={{ width: "500px" }}>
-            <label className='label' for='searchBar'>Search by name: </label>
+            <label className='label' htmlFor='searchBar'>Search by name: </label>
             <input type="search" className='input' id='searchBar' onChange={handleSearchByName} />
         </div>
     )
