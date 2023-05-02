b2307374e41707e2880ead72bc8d729b1b714286
Michauu035 <51509107+Michauu035@users.noreply.github.com>
2023-05-02 14:28:19 +0200

    add react-table

--- src/components/MovesList.jsx
+++ src/components/MovesList.jsx
@@ -2,39 +2,96 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import Pokedex from 'pokedex-promise-v2'
 import MovesListElement from './MovesListElement'
+import {useTable} from 'react-table'
 
 function MovesList() {
     const [movesList, setMovesList] = useState([])
+    const data = React.useMemo(
+        () => [
+          {
+            col1: 'Hello',
+            col2: 'World',
+          },
+          {
+            col1: 'react-table',
+            col2: 'rocks',
+          },
+          {
+            col1: 'whatever',
+            col2: 'you want',
+          },
+        ],
+        []
+      )
 
-    useEffect(() => {
-        const pokedex = new Pokedex()
-        pokedex.getMovesList().then((data) => {
-            setMovesList(data.results)
-        })
-    }, [])
+      const columns = React.useMemo(
+        () => [
+          {
+            Header: 'Column 1',
+            accessor: 'col1', // accessor is the "key" in the data
+          },
+          {
+            Header: 'Column 2',
+            accessor: 'col2',
+          },
+        ],
+        []
+      )
 
-    return (
-        <div>
-            <table className='table' width={'100%'}>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>Accuracy</th>
-                        <th>Power</th>
-                        <th><span data-tooltip="Power Points" className='has-tooltip-top has-tooltip-arrow'>PP</span></th>
-                        <th>Target</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {movesList.map((move, k) =>
-                        <MovesListElement name={move.name} url={move.url} key={k} />
-                    )}
-                </tbody>
-            </table>
-        </div>
-    )
+      const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({ columns, data })
+
+      return (
+        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th
+                    {...column.getHeaderProps()}
+                    style={{
+                      borderBottom: 'solid 3px red',
+                      background: 'aliceblue',
+                      color: 'black',
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td
+                        {...cell.getCellProps()}
+                        style={{
+                          padding: '10px',
+                          border: 'solid 1px gray',
+                          background: 'papayawhip',
+                        }}
+                      >
+                        {cell.render('Cell')}
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      )
 }
 
 export default MovesList
