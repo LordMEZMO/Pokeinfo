8995882d5d9b9ad202990d71c72e9d6e254889eb
Kajetan Bucholc <58823834+LordMEZMO@users.noreply.github.com>
2023-05-15 22:42:13 +0200

    compare button
    working compare

--- src/pages/Compare.js
+++ src/pages/Compare.js
@@ -1,5 +1,3 @@
-import '../com.css';
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -8,6 +6,9 @@ function PokemonSearch() {
   const [searchTermRight, setSearchTermRight] = useState("");
   const [suggestionsLeft, setSuggestionsLeft] = useState([]);
   const [suggestionsRight, setSuggestionsRight] = useState([]);
+  const [selectedPokemonLeft, setSelectedPokemonLeft] = useState(null);
+  const [selectedPokemonRight, setSelectedPokemonRight] = useState(null);
+  const [isCompared, setIsCompared] = useState(false);
 
   const handleChangeLeft = (event) => {
     setSearchTermLeft(event.target.value);
@@ -69,20 +70,37 @@ function PokemonSearch() {
       .catch((error) => console.log(error));
   }, [searchTermRight]);
 
-  const handleSuggestionClick = (suggestion, field) => {
-    if (field === "left") {
+  const handleSuggestionClick = (suggestion, side) => {
+    if (side === "left") {
       setSearchTermLeft(suggestion.name);
+      setSelectedPokemonLeft(suggestion);
       setSuggestionsLeft([]);
-    } else if (field === "right") {
+    } else if (side === "right") 
+    {
       setSearchTermRight(suggestion.name);
+      setSelectedPokemonRight(suggestion);      
       setSuggestionsRight([]);
     }
-  };
-
-  
+};
 
   const handleCompareClick = () => {
-    console.log(`Comparing ${searchTermLeft} with ${searchTermRight}`);
+    if (searchTermLeft && searchTermRight) {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${searchTermLeft.toLowerCase()}`)
+        .then((response) => {
+          setSelectedPokemonLeft(response.data);
+          setIsCompared(true);
+        })
+        .catch((error) => console.log(error));
+
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${searchTermRight.toLowerCase()}`)
+        .then((response) => {
+          setSelectedPokemonRight(response.data);
+          setIsCompared(true);
+        })
+        .catch((error) => console.log(error));
+    }
   };
 
   return (
@@ -100,52 +118,85 @@ function PokemonSearch() {
           {suggestionsLeft.map((suggestion, index) => (
             <div
               key={index}
-              onClick={() => handleSuggestionClick(suggestion, "left")}
+              onClick={() => handleSuggestionClick(suggestion,"left")}
               style={{
                 display: "flex",
                 alignItems: "center",
-                cursor: "pointer"
+                cursor: "pointer",
               }}
             >
-             
-<img src={suggestion.imageUrl} alt={suggestion.name} />
-<span style={{ marginLeft: "10px" }}>{suggestion.name}</span>
-</div>
-))}
-</div>
-
-</div>
-<button onClick={handleCompareClick}>Porównaj</button>
-<div>
-<label htmlFor="searchRight">Wyszukaj pokemona po prawej stronie:</label>
-<input
-       type="text"
-       id="searchRight"
-       name="searchRight"
-       value={searchTermRight}
-       onChange={handleChangeRight}
-     />
-<div>
-
-{suggestionsRight.map((suggestion, index) => (
-<div
-key={index}
-onClick={() => handleSuggestionClick(suggestion, "right")}
-style={{
-display: "flex",
-alignItems: "center",
-cursor: "pointer",
-}}
->
-<img src={suggestion.imageUrl} alt={suggestion.name} />
-<span style={{ marginLeft: "10px" }}>{suggestion.name}</span>
-</div>
-))}
-</div>
-</div>
-
-</div>
-);
+              <img src={suggestion.imageUrl} alt={suggestion.name} />
+              <span>{suggestion.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <label htmlFor="searchRight">Wyszukaj pokemona po prawej stronie:</label>
+        <input
+          type="text"
+          id="searchRight"
+          name="searchRight"
+          value={searchTermRight}
+          onChange={handleChangeRight}
+        />
+        <div>
+          {suggestionsRight.map((suggestion, index) => (
+            <div
+              key={index}
+              onClick={() => handleSuggestionClick(suggestion,"right")}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                cursor: "pointer",
+              }}
+            >
+              <img src={suggestion.imageUrl} alt={suggestion.name} />
+              <span>{suggestion.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <button onClick={handleCompareClick}>Porównaj</button>
+      </div>
+      <div>
+        {isCompared && selectedPokemonLeft && (
+          <div>
+            <h3>{selectedPokemonLeft.name}</h3>
+            <img
+              src={selectedPokemonLeft.sprites.front_default}
+              alt={selectedPokemonLeft.name}
+            />
+            <p>Waga: {selectedPokemonLeft.weight}</p>
+            <p>Wzrost: {selectedPokemonLeft.height}</p>
+            <p>
+              Typy:{" "}
+              {selectedPokemonLeft.types.map((type) => type.type.name).join(", ")}
+            </p>
+          </div>
+        )}
+      </div>
+      <div>
+        {isCompared && selectedPokemonRight && (
+          <div>
+            <h3>{selectedPokemonRight.name}</h3>
+            <img
+              src={selectedPokemonRight.sprites.front_default}
+              alt={selectedPokemonRight.name}
+            />
+            <p>Waga: {selectedPokemonRight.weight}</p>
+            <p>Wzrost: {selectedPokemonRight.height}</p>
+            <p>
+              Typy:{" "}
+              {selectedPokemonRight.types.map((type) => type.type.name).join(", ")}
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default PokemonSearch;
+
